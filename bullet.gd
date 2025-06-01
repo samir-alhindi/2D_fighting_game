560483224c820215526e0fde895e4c2972756385
buzzbuzz20xx <samirskyland@gmail.com>
2025-06-01 18:36:30 +0300

    added health
    and fixed bug with player shooting themselves

--- bullet.gd
+++ bullet.gd
@@ -1,4 +1,4 @@
-extends Area2D
+class_name Bullet extends Area2D
 
 var speed: int = 900
 ## Left or right.
@@ -7,3 +7,7 @@ var dir: int
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	global_position.x += speed * delta * dir
+
+
+func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
+	queue_free()                
--- fighter.gd
+++ fighter.gd
@@ -2,10 +2,12 @@ extends CharacterBody2D
 
 @export_enum("p1_", "p2_") var num: String = "p1_"
 
-const SPEED = 500.0
-const JUMP_VELOCITY = -900.0
-const accel = 900
-const friction = 1000
+var speed: float = 500.0
+var jump_force: float = -900.0
+var accel = 900
+var friction = 1000
+
+var health: int = 100
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -14,18 +16,20 @@ func _physics_process(delta: float) -> void:
 
 	# Handle jump.
 	if Input.is_action_just_pressed(num + "jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+		velocity.y = jump_force
 	
 	
 	# movement:
 	var dir: float = Input.get_axis(num+"left", num+"right")
 	if dir != 0:
-		velocity.x = SPEED * dir
-		#velocity.x = move_toward(velocity.x, SPEED * dir, accel * delta)
+		velocity.x = speed * dir
+		#velocity.x = move_toward(velocity.x, speed * dir, accel * delta)
 		if dir > 0:
-			$Sprite/Nose.position = abs($Sprite/Nose.position)
+			%Nose.position.x = 46.0
+			%ShootingPoint.position.x = 72.0
 		elif dir < 0:
-			$Sprite/Nose.position = abs($Sprite/Nose.position) * -1
+			%Nose.position.x = -46.0
+			%ShootingPoint.position.x = -72.0
 	else:
 		velocity.x = 0
 		#velocity.x = move_toward(velocity.x, 0, friction * delta)
@@ -33,10 +37,19 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_pressed(num+"shoot") and %CooldownTimer.is_stopped():
 		%CooldownTimer.start()
 		const BULLET: PackedScene = preload("uid://ciuw68m2kqgis")
-		var bullet: Area2D = BULLET.instantiate()
+		var bullet: Bullet = BULLET.instantiate()
 		%Bullets.add_child(bullet)
 		bullet.global_position = %ShootingPoint.global_position
-		bullet.dir = 1 if $Sprite/Nose.position.xa > 0 else -1
+		bullet.dir = 1 if %Nose.position.x > 0 else -1
 	
 	
 	move_and_slide()
+
+
+func _on_hurt_box_area_entered(area: Area2D) -> void:
+	if area is Bullet:
+		area.queue_free()
+		health -= 10
+		%HealthLabel.text = "HP: " + str(health)
+		if health <= 0:
+			queue_free()
