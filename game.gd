88574bc42e44cda41c3b07ed2740d15ef1357138
buzzbuzz20xx <samirskyland@gmail.com>
2025-06-05 19:40:29 +0300

    Added music and made game good

--- game.gd
+++ game.gd
@@ -12,11 +12,11 @@ func _ready() -> void:
 			add_child(instance)
 			%DeathSound.play()
 			if who == "p1_":
-				%WonLabel.text = "Blue wins !!!"
+				%WonLabel.text = "Blue wins !"
 				%WonLabel.modulate = Color(0.0, 0.0, 255.0)
 				instance.color = Color(0.0, 0.0, 255.0)
 			elif who == "p2_":
-				%WonLabel.text = "Red wins !!!"
+				%WonLabel.text = "Red wins !"
 				%WonLabel.modulate = Color(255.0, 0.0, 0.0)
 				instance.color = Color(255.0, 0.0, 0.0)
 			%WonLabel.show()
